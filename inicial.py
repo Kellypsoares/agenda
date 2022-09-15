59eb4037010adcbf1ec5728a0d193341e43b6327
Kellypsoares <kellypinheirosoares@poli.ufrj.br>
2022-09-15 15:54:44 -0300

    oi

---
+++ inicial.py
@@ -0,0 +1,20 @@
+
+from flask import Flask
+
+
+app = Flask(__name__)
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+
+@app.route("/<mensagem>")
+def hello_world2(mensagem):
+    return f"<p>Hello, World! {mensagem}</p>"
+
+
+
+
+if __name__ == '__main__':
+    app.run()
